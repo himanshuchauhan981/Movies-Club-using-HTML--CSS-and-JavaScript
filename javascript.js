63f021f4897166b7446e7175d1191c972aeff6b7
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-05 09:51:17 +0530

    Bugs Fixed related to adding new Movies

--- javascript.js
+++ javascript.js
@@ -6,29 +6,17 @@ function addNewMovie(){
    new_div.setAttribute("class","movie-list-style");
    new_div.setAttribute("id","new_id");
    list.append(new_div);
-
+   var new_span = document.createElement("span");
    var list1 = document.querySelectorAll('#new_id');
-   if(list1.length == 1){
-      var new_span = document.createElement("span");
-      new_div.appendChild(new_span);
-      new_span.appendChild(document.createTextNode(movie_name));
-
-      var new_button = document.createElement("button");
-      new_div.appendChild(new_button);
-      new_button.setAttribute("class","movie-delete");
-      new_button.appendChild(document.createTextNode("Delete"));
-   }
-   else{
-      var new_span = document.createElement("span");
+   if(list1.length != 1){
       new_div = list1[list1.length-1];
-      new_div.appendChild(new_span);
-      new_span.appendChild(document.createTextNode(movie_name));
-
-      var new_button = document.createElement("button");
-      new_div.appendChild(new_button);
-      new_button.setAttribute("class","movie-delete");
-      new_button.appendChild(document.createTextNode("Delete"));
    }
+   new_div.appendChild(new_span);
+   new_span.appendChild(document.createTextNode(movie_name));
+   var new_button = document.createElement("button");
+   new_div.appendChild(new_button);
+   new_button.setAttribute("class","movie-delete");
+   new_button.appendChild(document.createTextNode("Delete"));
 }
 
 
